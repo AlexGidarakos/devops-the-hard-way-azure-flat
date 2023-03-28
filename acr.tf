11eea8bb0ddd1cf350cb550901542baa544089c2
Alexandros Gidarakos <alexandros.gidarakos@kainos.com>
2023-03-28 16:56:11 +0100

    Fix minor indentation mismatch

--- acr.tf
+++ acr.tf
@@ -12,7 +12,7 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Standard"
   admin_enabled       = false
 
-# Push the Docker image that was created by 040-create-aad-group-and-docker-image.sh
+  # Push the Docker image that was created by 040-create-aad-group-and-docker-image.sh
   provisioner "local-exec" {
     command = "az acr login --name ${self.name} && docker tag uberapp ${self.login_server}/uberapp:v1 && docker push ${self.login_server}/uberapp:v1"
   }
