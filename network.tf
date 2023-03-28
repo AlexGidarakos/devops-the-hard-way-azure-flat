48e94cf28ed9b9be4a6192f03b59874376a3e0d5
Alexandros Gidarakos <alexandros.gidarakos@kainos.com>
2023-03-28 17:48:41 +0100

    Fix indentation with terraform validate

--- network.tf
+++ network.tf
@@ -1,23 +1,23 @@
 # The VNET
 resource "azurerm_virtual_network" "virtual_network" {
-  name =  "${var.name}-vnet"
-  location = var.location
+  name                = "${var.name}-vnet"
+  location            = var.location
   resource_group_name = azurerm_resource_group.main_rg.name
-  address_space = [var.network_address_space]
+  address_space       = [var.network_address_space]
 }
 
 # The subnet for the AKS
 resource "azurerm_subnet" "aks_subnet" {
-  name = var.aks_subnet_address_name
+  name                 = var.aks_subnet_address_name
   resource_group_name  = azurerm_resource_group.main_rg.name
   virtual_network_name = azurerm_virtual_network.virtual_network.name
-  address_prefixes = [var.aks_subnet_address_prefix]
+  address_prefixes     = [var.aks_subnet_address_prefix]
 }
 
 # The subnet for the application
 resource "azurerm_subnet" "app_gwsubnet" {
-  name = var.subnet_address_name
+  name                 = var.subnet_address_name
   resource_group_name  = azurerm_resource_group.main_rg.name
   virtual_network_name = azurerm_virtual_network.virtual_network.name
-  address_prefixes = [var.subnet_address_prefix]
+  address_prefixes     = [var.subnet_address_prefix]
 }
