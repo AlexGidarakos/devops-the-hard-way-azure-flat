acb074818c57311962f62c83f940d06ecaa153cf
Alexandros Gidarakos <alexandros.gidarakos@kainos.com>
2023-03-28 13:43:22 +0100

    Add providers Terraform file

---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "RESOURCE_GROUP_NAME"
+    storage_account_name = "STORAGE_ACCOUNT_NAME"
+    container_name       = "STORAGE_CONTAINER_NAME"
+    key                  = "TFSTATE_FILENAME"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
