64df21eae258b6220fe747b7c637d5ebf5ad6a2a
Alexandros Gidarakos <alexandros.gidarakos@kainos.com>
2023-03-28 13:07:13 +0100

    Rename variable for tfstate Resource Group

--- 030-create-terraform-storage.sh
+++ 030-create-terraform-storage.sh
@@ -3,12 +3,12 @@
 source 020-shell-script-variables.sh.inc
 
 # Create Resource Group
-echo "Creating Resource Group $RESOURCE_GROUP_NAME in region $PROJECT_REGION ..."
-az group create -l $PROJECT_REGION -n $RESOURCE_GROUP_NAME
+echo "Creating Resource Group $TFSTATE_RESOURCE_GROUP_NAME in region $PROJECT_REGION ..."
+az group create -l $PROJECT_REGION -n $TFSTATE_RESOURCE_GROUP_NAME
 
 # Create Storage Account
 echo "Creating Storage Account $STORAGE_ACCOUNT_NAME ..."
-az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l $PROJECT_REGION --sku Standard_LRS
+az storage account create -n $STORAGE_ACCOUNT_NAME -g $TFSTATE_RESOURCE_GROUP_NAME -l $PROJECT_REGION --sku Standard_LRS
 
 # Create Storage Account blob
 echo "Creating Storage Account blob ..."
