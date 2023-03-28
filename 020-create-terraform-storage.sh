56f2019efae7208548d991dd274a0196c822fefc
Alexandros Gidarakos <alexandros.gidarakos@kainos.com>
2023-03-28 11:38:21 +0100

    Add shell script to create storage for remote TF state file

---
+++ 020-create-terraform-storage.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+source 010-shell-script-variables.sh.inc
+
+RESOURCE_GROUP_NAME="$PROJECT_NAME-tfstate-rg"
+# Following var value MUST be max 24 chars and include only lowercase letters and numbers
+STORAGE_ACCOUNT_NAME="${PROJECT_NAME}tfstate"
+
+# Create Resource Group
+echo "Creating Resource Group $RESOURCE_GROUP_NAME in region $PROJECT_REGION ..."
+az group create -l $PROJECT_REGION -n $RESOURCE_GROUP_NAME
+
+# Create Storage Account
+echo "Creating Storage Account $STORAGE_ACCOUNT_NAME ..."
+az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l $PROJECT_REGION --sku Standard_LRS
+
+# Create Storage Account blob
+echo "Creating Storage Account blob ..."
+az storage container create  --name tfstate --account-name $STORAGE_ACCOUNT_NAME
