ec80570811d900388203ad82068b43efb0140e1c
Alexandros Gidarakos <alexandros.gidarakos@kainos.com>
2023-03-28 14:05:18 +0100

    Add block that replaces placeholder backend config values present in providers.tf

--- 030-create-terraform-storage.sh
+++ 030-create-terraform-storage.sh
@@ -12,4 +12,11 @@ az storage account create -n $STORAGE_ACCOUNT_NAME -g $TFSTATE_RESOURCE_GROUP_NA
 
 # Create Storage Account blob
 echo "Creating Storage Account blob ..."
-az storage container create  --name $STORAGE_CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+az storage container create --name $STORAGE_CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+
+# Replace placeholder backend config values in providers.tf
+echo "Replacing placeholder backend config values in providers.tf ..."
+sed -i "" "s/RESOURCE_GROUP_NAME/$RESOURCE_GROUP_NAME/"       providers.tf
+sed -i "" "s/STORAGE_ACCOUNT_NAME/$STORAGE_ACCOUNT_NAME/"     providers.tf
+sed -i "" "s/STORAGE_CONTAINER_NAME/$STORAGE_CONTAINER_NAME/" providers.tf
+sed -i "" "s/TFSTATE_FILENAME/$TFSTATE_FILENAME/"             providers.tf
