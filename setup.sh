d8f368a6f13a61595cedc1692cdb3e77f987a4b2
Alexandros Gidarakos <alexandros.gidarakos@kainos.com>
2023-03-30 14:40:06 +0100

    Debugging GH Actions #5

--- setup.sh
+++ setup.sh
@@ -10,22 +10,38 @@ if [[ "$NOTFOUND" == "true" ]]; then
   echo "Please install and/or add unmet requirements to the PATH variable and try again"
   exit 1
 else
-  echo "All requirements met, proceeding..."
+  echo "All requirements met"
 fi
 
 # If not running via GH Actions and secrets file is not present, then create a
 # Service Principal for CI/CD and store relevant secrets in a Git-ignored file
-[[ "$GITHUB_ACTIONS" != "true" ]] && [ ! -f "$CI_CD_SECRETS_FILE" ] && {
-  echo "Creating Service Principal for CI/CD";
-  AZ_SUB=$(az account show --query id --output tsv);
-  AZ_AD_SP_OUTPUT=$(az ad sp create-for-rbac \
-    --name "$PROJECT_NAME" \
-    --role contributor \
-    --scopes /subscriptions/$AZ_SUB \
-    --sdk-auth \
-    ) && {
-    echo "$AZ_AD_SP_OUTPUT" > $CI_CD_SECRETS_FILE && \
-    echo "Service Principal authentication JSON saved as $CI_CD_SECRETS_FILE" \
-    ;
-  }
-}
+echo "Checking if script is run by GitHub Actions"
+if [[ "$GITHUB_ACTIONS" == "true" ]]; then
+  echo "Run by GitHub Actions, skipping creation of Service Principal"
+else
+  echo "Not run by GitHub Actions"
+  echo "Checking if previous CI/CD secrets file exists"
+  if [[ -f "$CI_CD_SECRETS_FILE" ]]; then
+    echo "Found file "$CI_CD_SECRETS_FILE", skipping creation of Service Principal"
+  else
+    echo "No previous CI/CD secrets file found, proceeding"
+    echo "Retrieving Subscription ID"
+    AZ_SUB=$(az account show --query id --output tsv)
+    echo "Creating Service Principal for CI/CD"
+    AZ_AD_SP_OUTPUT=$(az ad sp create-for-rbac \
+      --name "$PROJECT_NAME" \
+      --role contributor \
+      --scopes /subscriptions/$AZ_SUB \
+      --sdk-auth \
+    )
+    if [[ $? -eq 0 ]]; then
+      echo "Service Principal for CI/CD created successfully"
+      echo "Saving Service Principal authentication JSON"
+      echo "$AZ_AD_SP_OUTPUT" > $CI_CD_SECRETS_FILE
+      echo "Service Principal authentication JSON saved as $CI_CD_SECRETS_FILE"
+    else
+      echo "Error creating Service Principal for CI/CD"
+      exit 2
+    fi
+  fi
+fi
