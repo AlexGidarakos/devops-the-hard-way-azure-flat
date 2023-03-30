2fba11fc4c55fe39d1736a582ca7866635a291f7
Alexandros Gidarakos <alexandros.gidarakos@kainos.com>
2023-03-30 12:43:28 +0100

    Add GHA Workflow step to login to Azure

--- setup.inc.sh
+++ setup.inc.sh
@@ -5,12 +5,13 @@ PROJECT_PREFIX="myname"
 # Check if running within a GitHub Actions runner
 # Based on https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
 # If true, prepend a "gh" string to the project prefix
-[[ "$GITHUB_ACTIONS" == "true" ]] && PROJECT_PREFIX="gh{$PROJECT_PREFIX}"
+[[ "$GITHUB_ACTIONS" == "true" ]] && PROJECT_PREFIX="gh${PROJECT_PREFIX}"
 
 PROJECT_REGION="uksouth"
 STORAGE_CONTAINER_NAME="tfstate"
 TFSTATE_FILENAME="terraform.tfstate"
 PROJECT_NAME="${PROJECT_PREFIX}${PROJECT_BASENAME}"
+CI_CD_SECRETS_FILE="cicd-secrets-gitignore.json"
 AKS_AAD_GROUP_NAME="$PROJECT_NAME-aks-group"
 TFSTATE_RESOURCE_GROUP_NAME="$PROJECT_NAME-tfstate-rg"
 # Following var value MUST be max 24 chars and include only lowercase letters and numbers
--- setup.sh
+++ setup.sh
@@ -6,16 +6,18 @@ for i in $REQUIREMENTS; do
   which $i > /dev/null && echo "$i found" || { echo "$i not found in PATH"; NOTFOUND=1; }
 done
 
-((NOTFOUND)) && { echo -e "Please install and/or add unmet requirements to the PATH variable and try again"; exit $NOTFOUND; }
+((NOTFOUND)) && { echo "Please install and/or add unmet requirements to the PATH variable and try again"; exit $NOTFOUND; }
 
-# Create Resource Group for Storage account to be used for the remote Terraform state
-echo "Creating Resource Group $TFSTATE_RESOURCE_GROUP_NAME in region $PROJECT_REGION ..."
-az group create -l $PROJECT_REGION -n $TFSTATE_RESOURCE_GROUP_NAME
-
-# Create the Storage Account
-echo "Creating Storage Account $STORAGE_ACCOUNT_NAME ..."
-az storage account create -n $STORAGE_ACCOUNT_NAME -g $TFSTATE_RESOURCE_GROUP_NAME -l $PROJECT_REGION --sku Standard_LRS
-
-# Create a blob within the Storage Account
-echo "Creating Storage Account blob ..."
-az storage container create --name $STORAGE_CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+# If not running via GH Actions and secrets file is not present, then create a
+# Service Principal for CI/CD and store relevant secrets in a Git-ignored file
+[[ "$GITHUB_ACTIONS" != "true" ]] && [ ! -f "$CI_CD_SECRETS_FILE" ] && {
+  echo "Creating Service Principal for CI/CD";
+  AZ_AD_SP_OUTPUT=$(az ad sp create-for-rbac \
+    --name "$PROJECT_NAME"\
+    --sdk-auth \
+    ) && {
+    echo "$AZ_AD_SP_OUTPUT" > $CI_CD_SECRETS_FILE && \
+    echo "Service Principal authentication JSON saved as $CI_CD_SECRETS_FILE" \
+    ;
+  }
+}
