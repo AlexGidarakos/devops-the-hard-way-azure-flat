cfddbccbdb7c6c054056590968a9df6edaf99696
Alexandros Gidarakos <alexandros.gidarakos@kainos.com>
2023-03-30 10:58:14 +0100

    Refactor script and script include logic

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ for i in $REQUIREMENTS; do
   which $i > /dev/null && echo "$i found" || { echo "$i not found in PATH"; NOTFOUND=1; }
 done
 
-((NOTFOUND)) && echo -e "Please install and/or add unmet requirements to the PATH variable and try again"
+((NOTFOUND)) && { echo -e "Please install and/or add unmet requirements to the PATH variable and try again"; exit $NOTFOUND; }
 
-exit $NOTFOUND
+echo "PROJECT_PREFIX=$PROJECT_PREFIX"
