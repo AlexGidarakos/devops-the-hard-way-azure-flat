82a9f83c14eb7ed5760a9e4f6decedef1f7f4edf
Alexandros Gidarakos <alexandros.gidarakos@kainos.com>
2023-03-30 10:25:39 +0100

    Fix return code issue and make code more compact

--- setup.1.sh
+++ setup.1.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
-
 source setup.inc
-
-# Check for required binaries
 NOTFOUND=0
-for i in $REQUIREMENTS;
-do
-  which $i > /dev/null
 
-  if [[ $? -eq 0 ]]; then
-    echo "$i found"
-  else
-    echo "$i not found in PATH"; NOTFOUND=1
-  fi
+for i in $REQUIREMENTS; do
+  which $i > /dev/null && echo "$i found" || { echo "$i not found in PATH"; NOTFOUND=1; }
 done
-((NOTFOUND)) && echo -e "Please install and/or add unmet requirements to the PATH variable and try again" && exit 1
+
+((NOTFOUND)) && echo -e "Please install and/or add unmet requirements to the PATH variable and try again"
+
+exit $NOTFOUND
